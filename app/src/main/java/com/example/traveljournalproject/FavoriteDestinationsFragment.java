890aa1b0522f86ce0307c7fad043a11d3d5445cd
Mihai Nicolae <mihainicolae2308@gmail.com>
2019-04-12 18:53:14 +0300

    Removed unused imports.

--- app/src/main/java/com/example/traveljournalproject/FavoriteDestinationsFragment.java
+++ app/src/main/java/com/example/traveljournalproject/FavoriteDestinationsFragment.java
@@ -2,8 +2,6 @@ package com.example.traveljournalproject;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
